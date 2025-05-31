f834ec31f90adf24a428a9ce9422cdb77e9b7082
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-05-31 19:08:33 +0530

    Another problem solved

---
+++ Arrays/src/MissingNumber.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args)
+    {
+        int[] arr = {3,0,1};
+        int ans = missingNumber(arr);
+        System.out.println(ans);
+    }
+    public static int missingNumber(int[] nums) {
+        int ans = 0;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++)
+        {
+            if(nums[i] != i)
+            {
+                ans = i;
+            }
+        }
+        return ans;
+    }
+}
