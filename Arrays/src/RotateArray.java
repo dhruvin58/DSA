3b9c48729c4971712fa2d175ef91191e33b0def6
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-06-02 19:18:33 +0530

    problem solved

--- Arrays/src/RotateArray.java
+++ Arrays/src/RotateArray.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class RotateArray {
+    public static void main(String[] args)
+    {
+        int[][] arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+    };
+        rotate(arr);
+    }
+    public static void rotate(int[][] matrix)
+    {   int n = matrix.length-1;
+//        for(int i=0; i<n; i++)
+//        {
+//            for(int j=0; j<n; j++)
+//            {
+//                for(int k=j; k<j+1; k++)
+//                {
+//                    matrix[k][n-i] = matrix[i][j];
+//                }
+//            }
+//        }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                matrix[i][j] = matrix[j][i];
+            }
+        }
+        for(int i=0; i<n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println(matrix[i][j]);
+            }
+        }
+    }
 }
