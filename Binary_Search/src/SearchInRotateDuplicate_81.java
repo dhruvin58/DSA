839c3e8245f6d6049dadc60b9ded119b61c444e7
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-06-10 10:33:53 +0530

    solve very beautiful problem of find in rotated array with duplicates values

---
+++ Binary_Search/src/SearchInRotateDuplicate_81.java
@@ -0,0 +1,51 @@
+public class SearchInRotateDuplicate_81 {
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 2, 3, 1};
+        int target = 1;
+        boolean ans = search(arr, target);
+        System.out.println(ans);  // ✅ true
+    }
+    static boolean search(int[] arr, int target)
+    {
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] == target)
+            {
+                return true;
+            }
+            if(arr[start] == arr[mid] && arr[mid] == arr[end])
+            {
+                start++;
+                end--;
+            }
+            else if(arr[start] <= arr[mid])
+            {
+                if(arr[start] <= target && target < arr[mid])
+                {
+                    end = mid-1;
+                }
+                else
+                {
+                    start = mid+1;
+                }
+            }
+            else
+            {
+                if(arr[mid] < target && target <= arr[end])
+                {
+                    start = mid+1;
+                }
+                else
+                {
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
