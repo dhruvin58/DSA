c6cd484f4588456a2e1cede0a76e7e3b9271baab
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-06-20 19:07:49 +0530

    leetcode problem solved

---
+++ Strings/src/ToLowercase_709.java
@@ -0,0 +1,11 @@
+public class ToLowercase_709 {
+    public static void main(String[] args)
+    {
+        String s = "Hello123";
+        String ans = toLowerCase(s);
+        System.out.println(ans);
+    }
+    static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
