6ac77df7e12cfb4c6288faaad157a0e04a60e8a4
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-06-22 13:20:55 +0530

    leetcode problem solved

---
+++ Strings/src/DefangIPaddr_1108.java
@@ -0,0 +1,10 @@
+public class DefangIPaddr_1108 {
+    public static void main(String[] args)
+    {
+        String address = "1.1.1.1";
+        System.out.println(defangIPaddr(address));
+    }
+    static String defangIPaddr(String address) {
+         return address.replace(".","[.]");
+    }
+}
