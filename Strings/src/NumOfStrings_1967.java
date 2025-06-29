d94403eb9f256848350239c3635957aae394a301
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-06-29 09:48:04 +0530

    some basic recursion problem solved

---
+++ Strings/src/NumOfStrings_1967.java
@@ -0,0 +1,20 @@
+public class NumOfStrings_1967 {
+    public static void main(String[] args) {
+        String[] patterns = {"a","abc","bc","d"};
+        String word = "abc";
+        System.out.println(numOfStrings(patterns,word));
+    }
+    static int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+
+        for (int i=0; i<patterns.length; i++)
+        {
+            if(word.contains(patterns[i]))
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
